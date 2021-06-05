5921abd34f0755ae72d771d0c4e75514e48ad7c1
spetseffect <spetseffect@gmail.com>
2021-06-05 21:07:26 +0300

    нарезал смайлики для аватарок; добавил адаптер для элемента списка пользователей; обновил методы Age() и setBirthday() в UserModel; слегка обновил вид элемента списка пользователей

---
+++ app/src/main/java/com/example/calc/UserListAdapter.java
@@ -0,0 +1,44 @@
+package com.example.calc;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class UserListAdapter extends ArrayAdapter<UserModel> {
+    private List<UserModel> list;
+    public UserListAdapter(@NonNull Context context, List<UserModel> list) {
+        super(context, R.layout.user_view_tpl,list);
+        this.list=list;
+    }
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        UserModel userli=list.get(position);
+        if(convertView==null){
+            convertView= LayoutInflater.from(getContext()).inflate(R.layout.user_view_tpl,parent,
+                    false);
+        }
+        ImageView iv = convertView.findViewById(R.id.avaUser);
+        iv.setImageResource(userli.Id());
+        TextView fName = convertView.findViewById(R.id.fName);
+        fName.setText(userli.firstName());
+        TextView lName = convertView.findViewById(R.id.lName);
+        lName.setText(userli.lastName());
+        TextView country = convertView.findViewById(R.id.country);
+        country.setText(userli.Country());
+        TextView city = convertView.findViewById(R.id.city);
+        city.setText(userli.City());
+        TextView birthday = convertView.findViewById(R.id.age);
+        birthday.setText(userli.Age());
+        return convertView;
+    }
+}
--- app/src/main/java/com/example/calc/UserModel.java
+++ app/src/main/java/com/example/calc/UserModel.java
@@ -1,21 +1,34 @@
 package com.example.calc;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Year;
+import java.util.Date;
+
 public class UserModel {
     private int id;
     private String fName;
     private String lName;
-    private long birthday;
+    private Date birthday;
     private String country;
     private String city;
-
-    public UserModel(int id, String fName, String lName, long birthday, String country, String city) {
-        this.id = id;
-        this.fName = fName;
-        this.lName = lName;
-        this.birthday = birthday;
-        this.country = country;
-        this.city = city;
-    }
+/*
+  в связи с разработкой под API 23, для вычисления возраста пришлось использовать
+  устаревшие методы класса Date.
+  A хотел использовать класс LocalDate (API 26).
+*/
+//    public UserModel(int id,String fName,String lName,long birthday,String country,String city) {
+//        this.id = id;
+//        this.fName = fName;
+//        this.lName = lName;
+//        --this.birthday = birthday;
+//        this.country = country;
+//        this.city = city;
+//    }
 
     public int Id() {return id;}
     public void setId(int id) {this.id = id;}
@@ -23,10 +36,20 @@ public class UserModel {
     public void setFirstName(String fName) {this.fName = fName;}
     public String lastName() {return lName;}
     public void setLastName(String lName) {this.lName = lName;}
-    public long getBirthday() {return birthday;}
-    public void setBirthday(long birthday) {this.birthday = birthday;}
-    public String getCountry() {return country;}
+    public String Age() {
+        long ageInMillis = new Date().getTime() - birthday.getTime();
+        Date age = new Date(ageInMillis);
+        return age.getYear()+" years old";
+//        long y = Year..until(birthday,LocalDate.now());
+//        return birthday;
+    }
+    public void setBirthday(int yyyy, int mm, int dd) {
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.set(yyyy,mm,dd,0,0,0);
+        this.birthday = new Date(yyyy,mm,dd);
+    }
+    public String Country() {return country;}
     public void setCountry(String country) {this.country = country;}
-    public String getCity() {return city;}
+    public String City() {return city;}
     public void setCity(String city) {this.city = city;}
 }
